4a80ffd0639c6347106fc82c7640a7b2cf557646
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-05-27 10:17:44 +0700

    first commit

---
+++ agregasi/ibu.h
@@ -0,0 +1,24 @@
+#ifndef IBU_H
+#define IBU_H
+
+class ibu {
+    public:
+        string name;
+        vector<anak*> daftar_anak;
+        ~ibu(string pNama): nama(pNama){
+            cout << "Ibu \""<< nama << "\" ada\n";
+            }
+
+        ~ibu(){
+            cout << "Ibu "" << nama << "\" tidak ada\n"
+            }
+        void tambahAnak(anak*);
+        void cetakAnak();
+};
+void ibu::tambahanAnak(anak* pAnak) {
+    daftar_anak.push_back(pAnak);
+}
+
+ibu::~ibu()
+{
+}
