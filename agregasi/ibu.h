f2415f1928ca46c8761b3ed2f7b7b1c61a966c96
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-05-27 12:02:42 +0700

    untuk membuat head untuk agregasi bagian ibu

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -1,24 +1,34 @@
 #ifndef IBU_H
 #define IBU_H
 
-class ibu {
+class ibu{
     public:
-        string name;
+        string nama;
         vector<anak*> daftar_anak;
-        ~ibu(string pNama): nama(pNama){
+
+        ibu(string pNama): nama(pNama){
             cout << "Ibu \""<< nama << "\" ada\n";
             }
 
         ~ibu(){
-            cout << "Ibu "" << nama << "\" tidak ada\n"
+            cout << "Ibu "" << nama << "\" tidak ada\n";
             }
-        void tambahAnak(anak*);
+        void tambahAnak(anak* pAnak);
         void cetakAnak();
 };
-void ibu::tambahanAnak(anak* pAnak) {
+void ibu::tambahAnak(anak* pAnak) {
     daftar_anak.push_back(pAnak);
 }
-
-ibu::~ibu()
+void ibu::cetakAnak()
 {
+    cout << "Daftar Anak dari ibu \"" << 
+    this->nama << "\":\n";
+    // for (auto& a: daftar_anak){
+    // cout << a->nama << "\n"
+    //}
+    for (int i = 0; i < daftar_anak.size(); i++) {
+        cout << daftar_anak[i]->nama<<endl;
+    }
+    cout << endl;
 }
+#endif
---
+++ komposisi/manusia.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+&nbsp;
+&nbsp;
+#include "jantung.h"
+#include "manusia.h"
+&nbsp;
+&nbsp;
+int main()
+{
+    manusia *varManusia = new manusia ("markus");
+}
