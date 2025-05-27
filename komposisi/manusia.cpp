eedbb9fb956e19093d2af4b4d620a9f6ef05dbc6
DhandhaDendriyaE <dandadendriya17@gmail.com>
2025-05-27 12:10:53 +0700

    membuat head manusia untuk dipanggil ke komposisi

--- komposisi/manusia.cpp
+++ komposisi/manusia.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <string>
 using namespace std;
-&nbsp;
-&nbsp;
 #include "jantung.h"
 #include "manusia.h"
-&nbsp;
-&nbsp;
+
 int main()
 {
     manusia *varManusia = new manusia ("markus");
--- komposisi/manusia.h
+++ komposisi/manusia.h
@@ -12,4 +12,5 @@ public:
         cout << name << " mati\n";
     }
 };
+
 #endif
